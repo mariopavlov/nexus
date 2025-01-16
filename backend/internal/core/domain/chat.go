cf660929e3ef20010f8fc40a65d3f4e88f540f0d
Mario Pavlov <marioppavlov@gmail.com>
2025-01-16 08:48:42 +0200

    MODEL: Chat Container

---
+++ backend/internal/core/domain/chat.go
@@ -0,0 +1,28 @@
+package domain
+
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type ChatID uuid.UUID
+
+type Chat struct {
+	ID        ChatID    `json:"id"`
+	Title     string    `json:"title"`
+	Messages  []Message `json:"messages"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+}
+
+func NewChat(title string) *Chat {
+	now := time.Now()
+
+	return &Chat{
+		ID:        ChatID{uuid.New()},
+		Title:     title,
+		CreatedAt: now,
+		UpdatedAt: now,
+	}
+}
